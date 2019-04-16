9e09b8abb7629e17ff04a480d7cd150056a585b0
Chen, Johnny <johnny.chen@optum.com>
2019-04-16 00:01:06 -0400

    q1.4 finished

---
+++ CTCI/q1_4.js
@@ -0,0 +1,35 @@
+/* Palindrome Permutation: Given a string, write a function to check if it is a permutation of a palindrome. */
+
+// Input: string
+// Output: boolean
+
+/* Questions
+Do white space matter? Capitalization?
+*/
+
+/* Solution:
+Create a map of the characters and their occurances. If all occurances are even or all even and one odd, then any permutation is also a palindrome.
+Time: O(n)
+Space: O(n)
+*/
+
+let palipermu = function(string) {
+  let charMap = {};
+  string = string.toLowerCase();
+  for (let c of string) {
+    if (c == ' ') continue;
+    if (charMap[c]) { charMap[c]++; }
+    else { charMap[c] = 1; }
+  }
+  let numOdds = 0;
+  for (let char of Object.keys(charMap)) {
+    if (charMap[char] % 2 != 0) { numOdds++; }
+  }
+  if (numOdds > 1) return false;
+  else return true;
+}
+
+console.log(palipermu('Taco cat'), true);
+console.log(palipermu('tac o tac'), true);
+console.log(palipermu('rrccaae'), true);
+console.log(palipermu('permumur'), false);
--- questions.md
+++ questions.md
@@ -1,5 +1,6 @@
-Question ID | Start Date | Answer Location
+Question ID | Completed Date | Answer Location
 ------------|------------|----------------
 CTCI-1.1 | 4/7/19 | CTCI/q1_1.js
 CTCI-1.2 | 4/15/19 | CTCI/q1_2.js
 CTCI-1.3 | 4/15/19 | CTCI/q1_3.js
+CTCI-1.4 | 4/15/19 | CTCI/q1_4.js
